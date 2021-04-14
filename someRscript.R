4d6718b602342a18f14d94ea649a601666187ea2
Ingmar Visser <47030842+ingmarvisser@users.noreply.github.com>
2021-04-14 11:01:11 +0200

    commiting changes now

--- someRscript.R
+++ someRscript.R
@@ -1,3 +1,5 @@
 #adding a little R script to the hello-world try-out
 x <- rnorm(100)
 plot(x)
+summary(x)
+x <- x+1
---
+++ someRscript2.R
@@ -0,0 +1,3 @@
+# more R code needed in this project
+y <- rnorm(100)
+density(y,plot=TRUE)
